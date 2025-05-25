65ecd0f1c35ad1d1344c4ad61d27d651a0ffd9f7
mraaan•🍉 <amr.bin.an@gmail.com>
2025-05-25 10:16:30 +0700

    add .editorconfig and format all of files

--- app/src/main/java/com/compose/jello/MainActivity.kt
+++ app/src/main/java/com/compose/jello/MainActivity.kt
@@ -14,34 +14,34 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.compose.jello.ui.theme.JelloTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            JelloTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      JelloTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+          Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+          )
         }
+      }
     }
+  }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+  Text(
+    text = "Hello $name!",
+    modifier = modifier
+  )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    JelloTheme {
-        Greeting("Android")
-    }
+  JelloTheme {
+    Greeting("Android")
+  }
 }
